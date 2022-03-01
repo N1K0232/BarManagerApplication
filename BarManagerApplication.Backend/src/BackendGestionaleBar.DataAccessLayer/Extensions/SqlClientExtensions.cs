65b6db27d34da1029360e545e4217babb7e09420
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-01 22:03:59 +0100

    added SqlClientExtensions class

---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.DataAccessLayer/Extensions/SqlClientExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace BackendGestionaleBar.DataAccessLayer.Extensions
+{
+    public static class SqlClientExtensions
+    {
+        public static Task<int> FillAsync(this SqlDataAdapter adapter, DataTable dataTable)
+        {
+            int result = adapter.Fill(dataTable);
+            return Task.FromResult(result);
+        }
+    }
+}
