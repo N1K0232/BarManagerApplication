4437b9f6e4d014734096f38762e01a94b5d5777b
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-01 21:55:37 +0100

    added CheckResult class used when the user tries to login

---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.Security/Models/CheckResult.cs
@@ -0,0 +1,14 @@
+﻿namespace BackendGestionaleBar.Security.Models
+{
+    public class CheckResult
+    {
+        public CheckResult(bool verified, bool needsUpgrade)
+        {
+            Verified = verified;
+            NeedsUpgrade = needsUpgrade;
+        }
+
+        public bool Verified { get; }
+        public bool NeedsUpgrade { get; }
+    }
+}
