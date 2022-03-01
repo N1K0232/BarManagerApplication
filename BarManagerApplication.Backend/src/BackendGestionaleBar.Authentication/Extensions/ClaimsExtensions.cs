0d6438c1c177f61bee99ac3e60fc1f8b4b4f9f74
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-01 21:42:07 +0100

    added ClaimsExtensions class

---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.Authentication/Extensions/ClaimsExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Security.Claims;
+using System.Security.Principal;
+
+namespace BackendGestionaleBar.Authentication.Extensions
+{
+    public static class ClaimsExtensions
+    {
+        public static Guid GetId(this IPrincipal user)
+        {
+            string value = GetClaimValue(user, ClaimTypes.NameIdentifier);
+            return Guid.Parse(value);
+        }
+
+        private static string GetClaimValue(IPrincipal user, string claimType)
+        {
+            string value = ((ClaimsPrincipal)user).FindFirst(claimType)?.Value;
+            return value;
+        }
+    }
+}
