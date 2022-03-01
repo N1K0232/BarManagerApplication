448dd601bc24a8eee980736a6572c5a8f76a031f
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-01 21:02:54 +0100

    imported database schema

---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.Database/dbo/Tables/Clienti.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Clienti] (
+    [Id]            UNIQUEIDENTIFIER NOT NULL,
+    [Nome]          NVARCHAR (MAX)   NOT NULL,
+    [Cognome]       NVARCHAR (MAX)   NOT NULL,
+    [DataNascita]   DATE             NOT NULL,
+    [CodiceFiscale] NVARCHAR (50)    NOT NULL,
+    [Telefono]      NVARCHAR (10)    NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    UNIQUE NONCLUSTERED ([CodiceFiscale] ASC),
+    UNIQUE NONCLUSTERED ([Telefono] ASC)
+);
+
