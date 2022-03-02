a8d35ccc1bf1e27e408716a48a4d7fd22c592deb
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-02 18:23:17 +0100

    added CheckPasswordRequest file

---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.Security/Models/Request/CheckPasswordRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace BackendGestionaleBar.Security.Models.Request
+{
+    public class CheckPasswordRequest
+    {
+        public string Hash { get; set; }
+        public string Password { get; set; }
+    }
+}
