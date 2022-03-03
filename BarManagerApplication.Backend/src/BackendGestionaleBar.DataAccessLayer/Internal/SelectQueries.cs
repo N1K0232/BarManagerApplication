550fc916358776425d8c38250881a2fc64ba2a47
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-03 21:05:44 +0100

    added class to manage all the SELECT query

---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.DataAccessLayer/Internal/SelectQueries.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+namespace BackendGestionaleBar.DataAccessLayer.Internal
+{
+    internal static class SelectQueries
+    {
+        public static string GetCliente()
+        {
+            StringBuilder builder = new();
+            builder.AppendLine("SELECT * FROM Clienti");
+            builder.AppendLine("WHERE IdCliente=@IdCliente");
+            return builder.ToString();
+        }
+    }
+}
