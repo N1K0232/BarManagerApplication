5834665829643a6b5cfb2f50750782c3bcc2ff78
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-05 13:25:33 +0100

    added GetCliente method

--- BarManagerApplication.Backend/src/BackendGestionaleBar/Controllers/ClientiController.cs
+++ BarManagerApplication.Backend/src/BackendGestionaleBar/Controllers/ClientiController.cs
@@ -1,6 +1,7 @@
 ﻿using BackendGestionaleBar.BusinessLayer.Services;
 using BackendGestionaleBar.Shared.Models.Requests;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Threading.Tasks;
 
 namespace BackendGestionaleBar.Controllers
@@ -17,13 +18,42 @@ namespace BackendGestionaleBar.Controllers
             this.clienteService = clienteService;
         }
 
+        [HttpGet("GetCliente")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> GetCliente(Guid idCliente)
+        {
+            if (idCliente == Guid.Empty)
+            {
+                return BadRequest("Invalid id");
+            }
+
+            var cliente = await clienteService.GetClienteAsync(idCliente);
+            if (cliente == null)
+            {
+                return NotFound("il cliente non esiste");
+            }
+            else
+            {
+                return Ok(cliente);
+            }
+        }
+
         [HttpPost("RegisterCliente")]
-        [ProducesResponseType(204)]
-        //[ProducesResponseType(200)]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
         public async Task<IActionResult> RegisterCliente([FromBody] RegisterClienteRequest request)
         {
             var response = await clienteService.RegisterClienteAsync(request);
-            return NoContent();
+            if (response.Succeeded)
+            {
+                return Ok("cliente registrato con successo");
+            }
+            else
+            {
+                return BadRequest(response);
+            }
         }
     }
 }
