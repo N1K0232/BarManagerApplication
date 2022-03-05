bb20669a97196818bfe786973f01d655d98bf268
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-05 09:26:42 +0100

    added initial registration of the customer

---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.BusinessLayer/Services/ClienteService.cs
@@ -0,0 +1,66 @@
+﻿using BackendGestionaleBar.DataAccessLayer;
+using BackendGestionaleBar.DataAccessLayer.Entities;
+using BackendGestionaleBar.Shared.Helpers;
+using BackendGestionaleBar.Shared.Models.Requests;
+using BackendGestionaleBar.Shared.Models.Responses;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BackendGestionaleBar.BusinessLayer.Services
+{
+    public class ClienteService : IClienteService
+    {
+        private readonly IDatabase db;
+
+        public ClienteService(IDatabase db)
+        {
+            this.db = db;
+        }
+
+        public async Task<Response> RegisterClienteAsync(RegisterClienteRequest request)
+        {
+            List<string> errors = ParametersHelper.CheckRegisterClienteParameters(request);
+            if (errors.Count > 0)
+            {
+                return new Response
+                {
+                    Succeeded = false,
+                    Errors = errors
+                };
+            }
+
+            var cliente = new Cliente
+            {
+                IdCliente = Guid.NewGuid(),
+                Nome = request.Nome,
+                Cognome = request.Cognome,
+                DataNascita = request.DataNascita.Value,
+                CodiceFiscale = request.CodiceFiscale,
+                Telefono = request.Telefono
+            };
+
+            var result = await db.RegisterClienteAsync(cliente);
+
+            if (result > 0)
+            {
+                return new Response
+                {
+                    Succeeded = true,
+                    Errors = null
+                };
+            }
+            else
+            {
+                return new Response
+                {
+                    Succeeded = false,
+                    Errors = new List<string>
+                    {
+                        "Errors during registration"
+                    }
+                };
+            }
+        }
+    }
+}
