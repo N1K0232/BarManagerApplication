e7245ae6a48c26e1bc6e3685be0ada7746a2f704
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-05 09:25:49 +0100

    added response model

---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.Shared/Models/Responses/Response.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace BackendGestionaleBar.Shared.Models.Responses
+{
+    public class Response
+    {
+        public bool Succeeded { get; set; }
+
+        public List<string> Errors { get; set; }
+    }
+}
