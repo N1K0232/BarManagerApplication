3d940d21ec76b3984f225f6d9e0033c2ada3d084
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-06 16:01:20 +0100

    added ApplicationDbContext

---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.DataAccessLayer/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BackendGestionaleBar.DataAccessLayer
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
