b4f90c73e7220872ccf8b490fe77fa405932f983
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-06 20:51:15 +0100

    added allergen

---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.DataAccessLayer/Configurations/AllergenConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using BackendGestionaleBar.DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BackendGestionaleBar.DataAccessLayer.Configurations
+{
+    public class AllergenConfiguration : IEntityTypeConfiguration<Allergen>
+    {
+        public void Configure(EntityTypeBuilder<Allergen> builder)
+        {
+            builder.ToTable("Allergens");
+
+            builder.HasKey(a => a.IdAllergen);
+            builder.Property(a => a.IdAllergen).UseIdentityColumn().IsRequired();
+
+            builder.Property(a => a.Name).HasMaxLength(50).IsRequired();
+            builder.Property(a => a.Description).HasMaxLength(512);
+        }
+    }
+}
