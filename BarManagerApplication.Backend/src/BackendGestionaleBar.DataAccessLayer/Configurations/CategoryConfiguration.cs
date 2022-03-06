33bcb3fe78764dc5900262a635ccc39249655f45
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-06 16:00:42 +0100

    added CategoryConfiguration

---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.DataAccessLayer/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using BackendGestionaleBar.DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BackendGestionaleBar.DataAccessLayer.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Categories");
+
+            builder.HasKey(c => c.IdCategory);
+            builder.Property(c => c.IdCategory).UseIdentityColumn(1, 1);
+
+            builder.Property(c => c.Name).HasMaxLength(256).IsRequired();
+            builder.Property(c => c.Description).HasMaxLength(512);
+        }
+    }
+}
