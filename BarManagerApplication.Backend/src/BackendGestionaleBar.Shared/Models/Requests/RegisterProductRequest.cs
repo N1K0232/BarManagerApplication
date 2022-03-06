dd8d820aeda5aeae0a6c183565eea08afa2a68ca
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-06 19:05:23 +0100

    added product registration

---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.Shared/Models/Requests/RegisterProductRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace BackendGestionaleBar.Shared.Models.Requests
+{
+    public class RegisterProductRequest
+    {
+        public int? IdCategory { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal? Price { get; set; }
+    }
+}
---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.Shared/Models/Responses/Response.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace BackendGestionaleBar.Shared.Models.Responses
+{
+    public class Response
+    {
+        public bool Succedeed { get; set; }
+        public List<string> Errors { get; set; }
+    }
+}
