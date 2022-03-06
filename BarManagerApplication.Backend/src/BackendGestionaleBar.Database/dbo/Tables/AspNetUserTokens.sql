176e31f5c39918d057b3084b20e6246f29a893bb
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-06 13:58:34 +0100

    added nuget packages and references

---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.Database/dbo/Tables/AspNetUserTokens.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[AspNetUserTokens] (
+    [UserId]        UNIQUEIDENTIFIER NOT NULL,
+    [LoginProvider] NVARCHAR (128)   NOT NULL,
+    [Name]          NVARCHAR (128)   NOT NULL,
+    [Value]         NVARCHAR (MAX)   NULL,
+    CONSTRAINT [PK_AspNetUserTokens] PRIMARY KEY CLUSTERED ([UserId] ASC, [LoginProvider] ASC, [Name] ASC)
+);
+
---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.Database/dbo/Tables/AspNetUsers.sql
@@ -0,0 +1,34 @@
+﻿CREATE TABLE [dbo].[AspNetUsers] (
+    [Id]                         UNIQUEIDENTIFIER   NOT NULL,
+    [UserName]                   NVARCHAR (256)     NULL,
+    [NormalizedUserName]         NVARCHAR (256)     NULL,
+    [FirstName]                  NVARCHAR (256)     NOT NULL,
+    [LastName]                   NVARCHAR (256)     NULL,
+    [BirthDate]                  DATETIME           NOT NULL,
+    [Email]                      NVARCHAR (256)     NULL,
+    [NormalizedEmail]            NVARCHAR (256)     NULL,
+    [EmailConfirmed]             BIT                NOT NULL,
+    [PasswordHash]               NVARCHAR (MAX)     NULL,
+    [SecurityStamp]              NVARCHAR (MAX)     NULL,
+    [ConcurrencyStamp]           NVARCHAR (MAX)     NULL,
+    [PhoneNumber]                NVARCHAR (MAX)     NULL,
+    [PhoneNumberConfirmed]       BIT                NOT NULL,
+    [TwoFactorEnabled]           BIT                NOT NULL,
+    [LockoutEnd]                 DATETIMEOFFSET (7) NULL,
+    [LockoutEnabled]             BIT                NOT NULL,
+    [AccessFailedCount]          INT                NOT NULL,
+    [RefreshToken]               NVARCHAR (512)     NULL,
+    [RefreshTokenExpirationDate] DATETIME           NULL,
+    CONSTRAINT [PK_AspNetUsers] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [EmailIndex]
+    ON [dbo].[AspNetUsers]([NormalizedEmail] ASC);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [UserNameIndex]
+    ON [dbo].[AspNetUsers]([NormalizedUserName] ASC) WHERE ([NormalizedUserName] IS NOT NULL);
+
