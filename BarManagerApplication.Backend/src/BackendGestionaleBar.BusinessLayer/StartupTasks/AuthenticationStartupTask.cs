15be191d8db92626888a39e9317dd9e1b695930c
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-06 15:29:31 +0100

    added AuthenticationStartupTask that creates the administrator and adds the roles in the database

---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.BusinessLayer/StartupTasks/AuthenticationStartupTask.cs
@@ -0,0 +1,66 @@
+﻿using BackendGestionaleBar.Authentication;
+using BackendGestionaleBar.Authentication.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BackendGestionaleBar.BusinessLayer.StartupTasks
+{
+    public class AuthenticationStartupTask : IHostedService
+    {
+        private readonly IServiceProvider serviceProvider;
+
+        public AuthenticationStartupTask(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            using var scope = serviceProvider.CreateScope();
+            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<ApplicationRole>>();
+
+            var roleNames = new string[] { RoleNames.Administrator, RoleNames.Cliente };
+
+            foreach (var roleName in roleNames)
+            {
+                var roleExists = await roleManager.RoleExistsAsync(roleName);
+                if (!roleExists)
+                {
+                    await roleManager.CreateAsync(new ApplicationRole(roleName));
+                }
+            }
+
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            var admin = new ApplicationUser
+            {
+                FirstName = "Nicola",
+                LastName = "Silvestri",
+                BirthDate = DateTime.Parse("22/10/2002"),
+                Email = "ns.nicolasilvestri@gmail.com",
+                UserName = "N1K0232"
+            };
+
+            await CheckCreateUserAsync(admin, "NicoSilve22!", RoleNames.Administrator, RoleNames.Cliente);
+
+            async Task CheckCreateUserAsync(ApplicationUser user, string password, params string[] roles)
+            {
+                var dbUser = await userManager.FindByEmailAsync(user.Email);
+                if (dbUser == null)
+                {
+                    var result = await userManager.CreateAsync(user, password);
+                    if (result.Succeeded)
+                    {
+                        await userManager.AddToRolesAsync(user, roles);
+                    }
+                }
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+            => Task.CompletedTask;
+    }
+}
