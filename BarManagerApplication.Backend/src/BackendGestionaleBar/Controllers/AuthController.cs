9c926123117fb5a1eb257a8df5763b627994e43b
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-06 15:29:47 +0100

    added the authentication controller

---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar/Controllers/AuthController.cs
@@ -0,0 +1,55 @@
+﻿using BackendGestionaleBar.BusinessLayer.Services;
+using BackendGestionaleBar.Shared.Models.Requests;
+using BackendGestionaleBar.Shared.Models.Responses;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BackendGestionaleBar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IIdentityService identityService;
+
+        public AuthController(IIdentityService identityService)
+        {
+            this.identityService = identityService;
+        }
+
+        [HttpPost("Login")]
+        [AllowAnonymous]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(AuthResponse))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+            var response = await identityService.LoginAsync(request);
+            if (response != null)
+            {
+                return Ok(response);
+            }
+
+            return BadRequest("email o password errati");
+        }
+
+        [HttpPost("Register")]
+        [AllowAnonymous]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+        {
+            var response = await identityService.RegisterUserAsync(request);
+            if (response.Succeeded)
+            {
+                return Ok("Utente registrato con successo");
+            }
+            else
+            {
+                return BadRequest(response.Errors);
+            }
+        }
+    }
+}
