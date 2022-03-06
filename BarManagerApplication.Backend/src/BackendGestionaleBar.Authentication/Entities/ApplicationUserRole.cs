845a7a510b7473369d76ecd4338abe0aa0744437
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-06 14:28:50 +0100

    configured authentication with users

---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.Authentication/Entities/ApplicationUserRole.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace BackendGestionaleBar.Authentication.Entities
+{
+    public class ApplicationUserRole : IdentityUserRole<Guid>
+    {
+        public virtual ApplicationUser User { get; set; }
+
+        public virtual ApplicationRole Role { get; set; }
+    }
+}
---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.Authentication/Extensions/ClaimsExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Claims;
+using System.Security.Principal;
+
+namespace BackendGestionaleBar.Authentication.Extensions
+{
+    public static class ClaimsExtensions
+    {
+        public static Guid GetId(this IPrincipal user)
+            => Guid.Parse(GetClaimValue(user, ClaimTypes.NameIdentifier));
+
+        public static string GetFirstName(this IPrincipal user)
+            => GetClaimValue(user, ClaimTypes.GivenName);
+
+        public static string GetLastName(this IPrincipal user)
+            => GetClaimValue(user, ClaimTypes.Surname);
+
+        public static string GetEmail(this IPrincipal user)
+            => GetClaimValue(user, ClaimTypes.Email);
+
+        public static DateTime GetBirthDate(this IPrincipal user)
+            => DateTime.Parse(GetClaimValue(user, ClaimTypes.DateOfBirth));
+
+        private static string GetClaimValue(IPrincipal user, string claimType)
+            => ((ClaimsPrincipal)user).FindFirst(claimType)?.Value;
+    }
+}
