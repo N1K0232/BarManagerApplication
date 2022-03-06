5aae429cc107adaa5cee22f1681b5d44dd4fbfae
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-06 16:05:27 +0100

    added references to tables with DbSet properties and overridden the OnModelCreating method

--- BarManagerApplication.Backend/src/BackendGestionaleBar.DataAccessLayer/ApplicationDbContext.cs
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.DataAccessLayer/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BackendGestionaleBar.DataAccessLayer.Configurations;
+using BackendGestionaleBar.DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace BackendGestionaleBar.DataAccessLayer
 {
@@ -7,6 +9,16 @@ namespace BackendGestionaleBar.DataAccessLayer
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
