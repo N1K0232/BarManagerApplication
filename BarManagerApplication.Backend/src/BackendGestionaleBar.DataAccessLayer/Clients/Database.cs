297df06950941e862191cb145b9950b14bad01c4
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-07 21:58:35 +0100

    added Database class and IDatabase interface

---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.DataAccessLayer/Clients/Database.cs
@@ -0,0 +1,87 @@
+﻿using BackendGestionaleBar.DataAccessLayer.Extensions;
+using BackendGestionaleBar.DataAccessLayer.Internal;
+using BackendGestionaleBar.Helpers;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace BackendGestionaleBar.DataAccessLayer.Clients
+{
+    public sealed partial class Database : IDatabase
+    {
+        private SqlConnection connection;
+        private SqlCommand command;
+        private SqlDataAdapter adapter;
+
+        public Database(string connectionStringHash)
+        {
+            InstanceConnection(connectionStringHash);
+        }
+
+        public async Task<DataTable> GetMenuAsync()
+        {
+            DataTable dataTable;
+            string query = QueryGenerator.GetMenu();
+
+            try
+            {
+                await connection.OpenAsync();
+                command = new SqlCommand(query, connection);
+                adapter = new SqlDataAdapter(command);
+                dataTable = new DataTable();
+                await adapter.FillAsync(dataTable);
+                await connection.CloseAsync();
+            }
+            catch (Exception)
+            {
+                dataTable = null;
+            }
+
+            return dataTable;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        private void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (connection != null)
+                {
+                    if (connection.State == ConnectionState.Open)
+                    {
+                        connection.Close();
+                    }
+                    connection.Dispose();
+                }
+                if (command != null)
+                {
+                    command.Dispose();
+                }
+                if (adapter != null)
+                {
+                    adapter.Dispose();
+                }
+            }
+        }
+        private void InstanceConnection(string connectionStringHash)
+        {
+            string connectionString = StringConverter.GetString(connectionStringHash);
+            connection = new SqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+                connection.Close();
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+        }
+    }
+}
