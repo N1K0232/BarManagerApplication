e973c9d4b5595747e0f5d8c01ecad55b96a4b35e
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-07 21:59:27 +0100

    created class to manage extensions methods for Microsoft.Data.SqlClient package

---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.DataAccessLayer/Extensions/SqlClientExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace BackendGestionaleBar.DataAccessLayer.Extensions
+{
+    public static class SqlClientExtensions
+    {
+        public static Task<int> FillAsync(this SqlDataAdapter adapter, DataTable dataTable)
+        {
+            int result;
+
+            try
+            {
+                result = adapter.Fill(dataTable);
+            }
+            catch (InvalidOperationException)
+            {
+                result = -1;
+            }
+
+            return Task.FromResult(result);
+        }
+    }
+}
