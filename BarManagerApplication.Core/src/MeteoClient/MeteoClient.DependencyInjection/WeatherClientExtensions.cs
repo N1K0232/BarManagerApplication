46459bebf20d5da7a0e9090b6264cb5b32b19dc2
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-08 21:50:35 +0100

    added MeteoClient.DependencyInjection project

---
+++ BarManagerApplication.Core/src/MeteoClient/MeteoClient.DependencyInjection/WeatherClientExtensions.cs
@@ -0,0 +1,14 @@
+﻿using MeteoClient.Core;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MeteoClient.DependencyInjection
+{
+    public static class WeatherClientExtensions
+    {
+        public static IServiceCollection AddWeatherClient(this IServiceCollection services)
+        {
+            services.AddScoped<IWeatherClient, WeatherClient>();
+            return services;
+        }
+    }
+}
