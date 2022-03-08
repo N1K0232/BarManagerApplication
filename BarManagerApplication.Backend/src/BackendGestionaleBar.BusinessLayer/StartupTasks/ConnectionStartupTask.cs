3a29244b225e3981fd87cdf20f15182b1defa8be
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-08 20:23:10 +0100

    added ConnectionStartupTask to test connection before launching the application

---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.BusinessLayer/StartupTasks/ConnectionStartupTask.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BackendGestionaleBar.BusinessLayer.StartupTasks
+{
+    public class ConnectionStartupTask : IHostedService
+    {
+        private readonly IServiceProvider serviceProvider;
+
+        public ConnectionStartupTask(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            using var connection = serviceProvider.GetRequiredService<SqlConnection>();
+
+            try
+            {
+                await connection.OpenAsync(cancellationToken);
+                await connection.CloseAsync();
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+            catch (InvalidOperationException)
+            {
+                throw;
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
