9e2eeddb605ac75021044528710ba7dc8a6026af
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-09 22:56:22 +0100

    added LoginAsync and GetUser async methods

--- BarManagerApplication.Core/src/BarApplication.APIClient/BarApplication.APIClient/Identity/Core/IIdentityClient.cs
+++ BarManagerApplication.Core/src/BarApplication.APIClient/BarApplication.APIClient/Identity/Core/IIdentityClient.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿using BarApplication.APIClient.Identity.Models;
+using BarApplication.APIClient.Identity.Models.Requests;
+using BarApplication.APIClient.Identity.Models.Responses;
+using System;
+using System.Threading.Tasks;
 
 namespace BarApplication.APIClient.Identity.Core
 {
     public interface IIdentityClient : IDisposable
     {
+        Task<User> GetUserAsync(string accessToken);
+        Task<AuthResponse> LoginAsync(LoginRequest request);
     }
 }
