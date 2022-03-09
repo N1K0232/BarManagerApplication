b3fe7e4a059f80e90c240c5085138b195f62eb35
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-09 23:18:02 +0100

    added HttpClientExtensions class to add services to the dependency injection system

---
+++ BarManagerApplication.Core/src/BarApplication.APIClient/BarApplication.APIClient.DependencyInjection/HttpClientExtensions.cs
@@ -0,0 +1,23 @@
+﻿using BarApplication.APIClient.Identity.Core;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http.Headers;
+
+namespace BarApplication.APIClient.DependencyInjection
+{
+    public static class HttpClientExtensions
+    {
+        public static IHttpClientBuilder AddIdentityClient(this IServiceCollection services)
+        {
+            var httpClientBuilder = services.AddHttpClient<IIdentityClient, IdentityClient>(httpClient =>
+            {
+                httpClient.BaseAddress = new Uri(Constants.BaseUrl);
+                httpClient.DefaultRequestHeaders.Accept.Clear();
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                return new IdentityClient(httpClient);
+            });
+
+            return httpClientBuilder;
+        }
+    }
+}
