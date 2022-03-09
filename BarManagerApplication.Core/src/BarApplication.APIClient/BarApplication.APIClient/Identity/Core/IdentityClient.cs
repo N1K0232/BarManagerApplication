f210cdd55acfd19f026230beb7b94e07ade50883
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-09 21:23:30 +0100

    initial create

---
+++ BarManagerApplication.Core/src/BarApplication.APIClient/BarApplication.APIClient/Identity/Core/IdentityClient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+
+namespace BarApplication.APIClient.Identity.Core
+{
+    public class IdentityClient : IIdentityClient
+    {
+        private readonly HttpClient httpClient;
+
+        public IdentityClient(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        private void Dispose(bool disposing)
+        {
+            if (disposing && httpClient != null)
+            {
+                httpClient.Dispose();
+            }
+        }
+    }
+}
---
+++ BarManagerApplication.Core/src/BarApplication.APIClient/BarApplication.APIClient/Identity/Models/Responses/AuthResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace BarApplication.APIClient.Identity.Models.Responses
+{
+    public class AuthResponse
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
