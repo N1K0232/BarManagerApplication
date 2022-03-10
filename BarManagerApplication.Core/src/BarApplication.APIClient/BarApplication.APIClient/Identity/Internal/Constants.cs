05e62c02c7a2b3bb0146a8f23aa76099534cfd2e
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-10 20:29:22 +0100

    added url string to access the UpdatePassword method

--- BarManagerApplication.Core/src/BarApplication.APIClient/BarApplication.APIClient/Identity/Internal/Constants.cs
+++ BarManagerApplication.Core/src/BarApplication.APIClient/BarApplication.APIClient/Identity/Internal/Constants.cs
@@ -6,6 +6,7 @@
         public const string RegisterClienteUrl = "api/Auth/RegisterCliente";
         public const string RegisterStaffUrl = "api/Auth/RegisterStaff";
         public const string RefreshTokenUrl = "api/Auth/Refresh";
+        public const string UpdatePasswordUrl = "api/Auth/UpdatePassword";
         public const string GetUserUrl = "/api/me";
         public const string Authorization = nameof(Authorization);
         public const string Bearer = nameof(Bearer);
