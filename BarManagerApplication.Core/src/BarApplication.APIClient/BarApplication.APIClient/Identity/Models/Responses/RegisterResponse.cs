502b5f6dfb2fa9f346c8b4c3717e71f7d7706f69
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-10 20:13:26 +0100

    added Response when the user is registrated

---
+++ BarManagerApplication.Core/src/BarApplication.APIClient/BarApplication.APIClient/Identity/Models/Responses/RegisterResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace BarApplication.APIClient.Identity.Models.Responses
+{
+    public class RegisterResponse
+    {
+        public bool Succeeded { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
