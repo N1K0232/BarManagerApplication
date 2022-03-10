720154f3d112a0f234ba7335f57325af1b71173c
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-10 20:13:04 +0100

    added constants to RegisterCliente and RegisterStaff api methods

--- BarManagerApplication.Core/src/BarApplication.APIClient/BarApplication.APIClient/Identity/Internal/Constants.cs
+++ BarManagerApplication.Core/src/BarApplication.APIClient/BarApplication.APIClient/Identity/Internal/Constants.cs
@@ -3,7 +3,8 @@
     internal static class Constants
     {
         public const string LoginUrl = "api/Auth/Login";
-        public const string RegisterUserUrl = "api/Auth/Register";
+        public const string RegisterClienteUrl = "api/Auth/RegisterCliente";
+        public const string RegisterStaffUrl = "api/Auth/RegisterStaff";
         public const string RefreshTokenUrl = "api/Auth/Refresh";
         public const string GetUserUrl = "/api/me";
         public const string Authorization = nameof(Authorization);
