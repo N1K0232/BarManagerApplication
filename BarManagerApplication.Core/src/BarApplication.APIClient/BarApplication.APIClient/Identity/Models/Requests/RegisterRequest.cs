739ce1db01f7196a1a98091d016e9b6f84bb4902
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-10 20:14:24 +0100

    added Request for registration of the user

---
+++ BarManagerApplication.Core/src/BarApplication.APIClient/BarApplication.APIClient/Identity/Models/Requests/RegisterRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BarApplication.APIClient.Identity.Models.Requests
+{
+    public class RegisterRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
