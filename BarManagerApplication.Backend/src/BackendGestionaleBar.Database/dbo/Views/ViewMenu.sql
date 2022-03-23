f6836b850f4efffaa680ef93d3e3e9f774cd0c1a
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-23 22:42:35 +0100

    fixed error

--- BarManagerApplication.Backend/src/BackendGestionaleBar.Database/dbo/Views/ViewMenu.sql
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.Database/dbo/Views/ViewMenu.sql
@@ -3,5 +3,5 @@ CREATE VIEW ViewMenu
 AS
 SELECT p.Name,p.Price,p.Quantity,c.Name as Category
 FROM Products p LEFT JOIN Categories c
-ON p.IdCategory=c.IdCategory
+ON p.IdCategory=c.Id
 WHERE p.Quantity > 0
