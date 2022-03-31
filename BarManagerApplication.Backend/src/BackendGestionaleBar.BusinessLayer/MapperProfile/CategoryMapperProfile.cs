0636fcac5795fe2bdab54a58a1bab88be3ff2c45
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-31 20:44:08 +0200

    added MapperProfiles

---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.BusinessLayer/MapperProfile/CategoryMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BackendGestionaleBar.Shared.Models;
+using ApplicationCategory = BackendGestionaleBar.DataAccessLayer.Entities.Category;
+
+namespace BackendGestionaleBar.BusinessLayer.MapperProfile
+{
+    public class CategoryMapperProfile : Profile
+    {
+        public CategoryMapperProfile()
+        {
+            CreateMap<ApplicationCategory, Category>();
+        }
+    }
+}
