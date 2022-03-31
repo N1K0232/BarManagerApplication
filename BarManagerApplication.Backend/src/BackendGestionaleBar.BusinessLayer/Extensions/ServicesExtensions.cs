c6427bcf91126c6b16ab8342c320d79032274235
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-31 20:45:17 +0200

    added services extensions methods

---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.BusinessLayer/Extensions/ServicesExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace BackendGestionaleBar.BusinessLayer.Extensions
+{
+    public static class ServicesExtensions
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            return services;
+        }
+        public static IServiceCollection AddHostedServices(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
