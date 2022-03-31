e8a69f3f2c348fb7229752d9655b73ac2eddb861
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-31 20:45:54 +0200

    updated models and entities

--- BarManagerApplication.Backend/src/BackendGestionaleBar.DataAccessLayer/Entities/Product.cs
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.DataAccessLayer/Entities/Product.cs
@@ -9,6 +9,8 @@ namespace BackendGestionaleBar.DataAccessLayer.Entities
 
         public string Name { get; set; }
 
+        public DateTime ExpirationDate { get; set; }
+
         public int Quantity { get; set; }
 
         public decimal Price { get; set; }
--- BarManagerApplication.Backend/src/BackendGestionaleBar.Shared/Models/Product.cs
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.Shared/Models/Product.cs
@@ -1,10 +1,19 @@
-﻿namespace BackendGestionaleBar.Shared.Models
+﻿using BackendGestionaleBar.Shared.Models.Common;
+using System;
+
+namespace BackendGestionaleBar.Shared.Models
 {
-    public class Product
+    public class Product : BaseModel
     {
+        public Guid IdCategory { get; set; }
+
         public Category Category { get; set; }
 
         public string Name { get; set; }
+
+        public int Quantity { get; set; }
+
+        public DateTime ExpirationDate { get; set; }
 
         public decimal Price { get; set; }
     }
