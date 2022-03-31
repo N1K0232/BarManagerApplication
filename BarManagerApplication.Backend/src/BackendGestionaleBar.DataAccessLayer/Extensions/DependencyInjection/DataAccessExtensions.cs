df003fbcad6e1d38d31bbf62ffdb69f25ee32492
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-31 17:35:53 +0200

    added DependencyInjection

---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.DataAccessLayer/Extensions/DependencyInjection/DataAccessExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace BackendGestionaleBar.DataAccessLayer.Extensions.DependencyInjection
+{
+    public static class DataAccessExtensions
+    {
+        public static IServiceCollection AddDataContext(this IServiceCollection services, Action<DataContextOptions> action)
+        {
+            var settings = new DataContextOptions();
+            action.Invoke(settings);
+            var connection = new SqlConnection(settings.ConnectionString);
+
+            services.AddScoped<IDataContext>(_ =>
+            {
+                var dataContext = new DataContext
+                {
+                    Connection = connection
+                };
+                return dataContext;
+            });
+
+            return services;
+        }
+    }
+}
