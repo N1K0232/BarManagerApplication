1ea930be98ca0d81057e3e97b2ef4df1abdb4e52
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-04-03 16:47:00 +0200

    added DependencyInjection to register my Database class

---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.DataAccessLayer/Extensions/DependencyInjection/DataAccessExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BackendGestionaleBar.DataAccessLayer.Extensions.DependencyInjection
+{
+    public static class DataAccessExtensions
+    {
+        public static IServiceCollection AddDatabase(this IServiceCollection services, Action<DatabaseOptionsBuilder> configuration)
+        {
+            services.AddSqlConnection(configuration);
+            services.AddScoped<IDatabase, Database>(serviceProvider =>
+            {
+                var connection = serviceProvider.GetRequiredService<SqlConnection>();
+                var database = new Database();
+                database.Connection = connection;
+                return database;
+            });
+            return services;
+        }
+
+        private static IServiceCollection AddSqlConnection(this IServiceCollection services, Action<DatabaseOptionsBuilder> configuration)
+        {
+            var builder = new DatabaseOptionsBuilder();
+            configuration.Invoke(builder);
+
+            services.AddScoped<SqlConnection>(_ =>
+            {
+                return new SqlConnection(builder.ConnectionString);
+            });
+
+            return services;
+        }
+    }
+}
