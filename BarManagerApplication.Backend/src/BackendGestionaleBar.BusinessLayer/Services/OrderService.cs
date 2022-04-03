13ed6f33539aff81834df9762c987270241ab3e5
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-04-03 21:18:57 +0200

    added OrderService

---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.BusinessLayer/Services/OrderService.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using BackendGestionaleBar.DataAccessLayer;
+
+namespace BackendGestionaleBar.BusinessLayer.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IDataContext dataContext;
+        private readonly IDatabase database;
+        private readonly IMapper mapper;
+
+        public OrderService(IDataContext dataContext, IDatabase database, IMapper mapper)
+        {
+            this.dataContext = dataContext;
+            this.database = database;
+            this.mapper = mapper;
+        }
+    }
+}
