367b054c6cf8939ab86506f282dbb30c7240c503
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-04-03 16:08:26 +0200

    created configurations

---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.DataAccessLayer/Configurations/Common/BaseEntityConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using BackendGestionaleBar.DataAccessLayer.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BackendGestionaleBar.DataAccessLayer.Configurations.Common
+{
+    internal abstract class BaseEntityConfiguration<T> : IEntityTypeConfiguration<T> where T : BaseEntity
+    {
+        public virtual void Configure(EntityTypeBuilder<T> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .IsRequired();
+
+            builder.Property(x => x.CreatedDate)
+                .IsRequired();
+
+            builder.Property(x => x.LastModifiedDate)
+                .IsRequired(false);
+        }
+    }
+}
