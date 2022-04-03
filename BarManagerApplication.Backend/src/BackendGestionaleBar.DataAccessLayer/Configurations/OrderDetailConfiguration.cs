e2da26cc5a3b538e97b5a4f08832de90f4b25576
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-04-03 20:15:28 +0200

    added OrderConfiguration and OrderDetailConfiguration

---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.DataAccessLayer/Configurations/OrderDetailConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using BackendGestionaleBar.DataAccessLayer.Configurations.Common;
+using BackendGestionaleBar.DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BackendGestionaleBar.DataAccessLayer.Configurations
+{
+    internal class OrderDetailConfiguration : BaseEntityConfiguration<OrderDetail>
+    {
+        public override void Configure(EntityTypeBuilder<OrderDetail> builder)
+        {
+            builder.ToTable("OrderDetails");
+
+            builder.HasKey(od => new { od.IdOrder, od.IdProduct });
+
+            builder.HasOne(od => od.Order)
+                .WithMany(o => o.OrderDetails)
+                .HasForeignKey(od => od.Order)
+                .IsRequired();
+
+            builder.HasOne(od => od.Product)
+                .WithMany(p => p.OrderDetails)
+                .HasForeignKey(od => od.IdProduct)
+                .IsRequired();
+        }
+    }
+}
