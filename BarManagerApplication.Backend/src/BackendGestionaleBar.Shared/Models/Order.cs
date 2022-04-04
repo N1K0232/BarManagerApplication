29c87c9c5de3de0cd603b8be04b7ea6345812775
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-04-04 12:07:42 +0200

    added Order response model and SaveOrderRequest model

---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.Shared/Models/Order.cs
@@ -0,0 +1,18 @@
+﻿using BackendGestionaleBar.Shared.Models.Common;
+using BackendGestionaleBar.Shared.Models.Enums;
+
+namespace BackendGestionaleBar.Shared.Models
+{
+    public class Order : BaseModel
+    {
+        public User User { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public TimeSpan OrderTime { get; set; }
+
+        public OrderStatus OrderStatus { get; set; }
+
+        public decimal TotalPrice { get; set; }
+    }
+}
---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.Shared/Models/Requests/SaveOrderRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace BackendGestionaleBar.Shared.Models.Requests
+{
+    public class SaveOrderRequest
+    {
+        public Guid? Id { get; set; }
+
+        public Guid IdProduct { get; set; }
+
+        public Guid IdUser { get; set; }
+    }
+}
