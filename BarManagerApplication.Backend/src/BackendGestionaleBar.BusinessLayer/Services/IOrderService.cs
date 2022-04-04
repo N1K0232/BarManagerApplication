4500219067042bb4b19b2846b757f3bfebfa3178
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-04-04 12:04:50 +0200

    added methods to OrderService

--- BarManagerApplication.Backend/src/BackendGestionaleBar.BusinessLayer/Services/IOrderService.cs
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.BusinessLayer/Services/IOrderService.cs
@@ -1,7 +1,13 @@
-﻿namespace BackendGestionaleBar.BusinessLayer.Services
+﻿using BackendGestionaleBar.Shared.Models;
+using BackendGestionaleBar.Shared.Models.Requests;
+
+namespace BackendGestionaleBar.BusinessLayer.Services
 {
     public interface IOrderService
     {
-
+        Task DeleteAsync();
+        Task DeleteAsync(Guid id);
+        Task<Order> GetOrderAsync(Guid id);
+        Task<Order> SaveAsync(SaveOrderRequest request);
     }
 }
--- BarManagerApplication.Backend/src/BackendGestionaleBar.BusinessLayer/Services/OrderService.cs
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.BusinessLayer/Services/OrderService.cs
@@ -1,5 +1,11 @@
 ﻿using AutoMapper;
 using BackendGestionaleBar.DataAccessLayer;
+using BackendGestionaleBar.Shared.Models;
+using BackendGestionaleBar.Shared.Models.Enums;
+using BackendGestionaleBar.Shared.Models.Requests;
+using Microsoft.EntityFrameworkCore;
+using ApplicationOrder = BackendGestionaleBar.DataAccessLayer.Entities.Order;
+using ApplicationOrderDetail = BackendGestionaleBar.DataAccessLayer.Entities.OrderDetail;
 
 namespace BackendGestionaleBar.BusinessLayer.Services
 {
@@ -14,6 +20,61 @@ namespace BackendGestionaleBar.BusinessLayer.Services
             this.dataContext = dataContext;
             this.database = database;
             this.mapper = mapper;
+        }
+
+        public async Task DeleteAsync()
+        {
+            var query = dataContext.GetData<ApplicationOrder>(trackingChanges: true);
+            query = query.Where(o => o.OrderStatus == OrderStatus.Canceled || o.OrderStatus == OrderStatus.Completed);
+            var orders = await query.ToListAsync();
+            dataContext.Delete(orders);
+            await dataContext.SaveAsync();
+        }
+        public async Task DeleteAsync(Guid id)
+        {
+            var order = await dataContext.GetAsync<ApplicationOrder>(id);
+            if (order != null)
+            {
+                dataContext.Delete(order);
+                await dataContext.SaveAsync();
+            }
+        }
+        public async Task<Order> SaveAsync(SaveOrderRequest request)
+        {
+            var dbOrder = request.Id != null ?
+                await dataContext.GetAsync<ApplicationOrder>(request.Id.Value)
+                : null;
+
+            if (dbOrder == null)
+            {
+                dbOrder = mapper.Map<ApplicationOrder>(request);
+                dbOrder.OrderStatus = OrderStatus.New;
+                dbOrder.OrderDate = DateTime.UtcNow;
+                dataContext.Insert(dbOrder);
+
+                var orderDetail = new ApplicationOrderDetail
+                {
+                    IdOrder = request.Id.Value,
+                    IdProduct = request.IdProduct
+                };
+
+                dataContext.Insert(orderDetail);
+            }
+            else
+            {
+                dbOrder.LastModifiedDate = DateTime.UtcNow;
+            }
+
+            await dataContext.SaveAsync();
+            return mapper.Map<Order>(dbOrder);
+        }
+        public async Task<Order> GetOrderAsync(Guid id)
+        {
+            var dbOrder = await dataContext.GetAsync<ApplicationOrder>(id);
+            var order = mapper.Map<Order>(dbOrder);
+            var totalPrice = await database.GetPriceAsync(id);
+            order.TotalPrice = totalPrice;
+            return order;
         }
     }
 }
