a4d7565d80f74bf33aed5f9bc8f21baf8bab99f6
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-04-04 21:07:57 +0200

    added Menu model class to fix DataTable error since I controllers can't return a DataTable object

---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.Shared/Models/Menu.cs
@@ -0,0 +1,10 @@
+﻿namespace BackendGestionaleBar.Shared.Models
+{
+    public class Menu
+    {
+        public string Name { get; init; }
+        public string Category { get; init; }
+        public decimal Price { get; init; }
+        public int Quantity { get; init; }
+    }
+}
