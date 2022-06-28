413dcad04a73ccbc3440dd08e48dc48a998ca1e3
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-06-28 21:03:54 +0200

    removed Startup class

--- BarManagerApplication.Backend/src/BackendGestionaleBar/Program.cs
+++ BarManagerApplication.Backend/src/BackendGestionaleBar/Program.cs
@@ -1,14 +1,156 @@
-namespace BackendGestionaleBar;
+using BackendGestionaleBar.Authentication;
+using BackendGestionaleBar.Authentication.Entities;
+using BackendGestionaleBar.Authorization.Handlers;
+using BackendGestionaleBar.Authorization.Requirements;
+using BackendGestionaleBar.BusinessLayer.Extensions;
+using BackendGestionaleBar.BusinessLayer.Services;
+using BackendGestionaleBar.BusinessLayer.Settings;
+using BackendGestionaleBar.BusinessLayer.StartupTasks;
+using BackendGestionaleBar.Contracts;
+using BackendGestionaleBar.DataAccessLayer;
+using BackendGestionaleBar.Services;
+using Hellang.Middleware.ProblemDetails;
+using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Net.Http.Headers;
+using Microsoft.OpenApi.Models;
+using Serilog;
+using System.Text;
+using System.Text.Json.Serialization;
+using TinyHelpers.Json.Serialization;
 
-public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((hostingContext, loggerConfiguration) =>
 {
-    public static async Task Main(string[] args)
+    loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration);
+});
+
+var jwtSettings = Configure<JwtSettings>(nameof(JwtSettings));
+
+builder.Services.AddProblemDetails();
+builder.Services.AddMapperProfiles();
+builder.Services.AddValidators();
+builder.Services.AddControllers()
+.AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
+    options.JsonSerializerOptions.Converters.Add(new UtcDateTimeConverter());
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "BackendGestionaleBar", Version = "v1" });
+    options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
     {
-        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-        Startup startup = new(builder.Services, builder.Configuration, builder.Host);
-        startup.ConfigureServices();
-        WebApplication application = builder.Build();
-        startup.Configure(application);
-        await application.RunAsync();
-    }
+        In = ParameterLocation.Header,
+        Description = "Insert the bearer token",
+        Name = HeaderNames.Authorization,
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = JwtBearerDefaults.AuthenticationScheme
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+})
+.AddFluentValidationRulesToSwagger(options =>
+{
+    options.SetNotNullableIfMinLengthGreaterThenZero = true;
+});
+
+string hash = builder.Configuration.GetConnectionString("SqlConnection");
+byte[] bytes = Convert.FromBase64String(hash);
+string connectionString = Encoding.UTF8.GetString(bytes);
+builder.Services.AddSqlServer<AuthenticationDataContext>(connectionString);
+builder.Services.AddSqlServer<DataContext>(connectionString);
+builder.Services.AddScoped<IDataContext>(services =>
+{
+    return services.GetRequiredService<DataContext>();
+});
+
+
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+{
+    options.User.RequireUniqueEmail = true;
+    options.Password.RequiredLength = 8;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+})
+.AddEntityFrameworkStores<AuthenticationDataContext>()
+.AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = jwtSettings.Issuer,
+        ValidateAudience = true,
+        ValidAudience = jwtSettings.Audience,
+        ValidateLifetime = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.SecurityKey)),
+        RequireExpirationTime = true,
+        ClockSkew = TimeSpan.Zero
+    };
+});
+builder.Services.AddAuthorization(options =>
+{
+    var policyBuilder = new AuthorizationPolicyBuilder().RequireAuthenticatedUser();
+    policyBuilder.Requirements.Add(new UserActiveRequirement());
+    options.FallbackPolicy = options.DefaultPolicy = policyBuilder.Build();
+});
+
+builder.Services.AddScoped<IAuthorizationHandler, UserActiveHandler>();
+builder.Services.AddScoped<IUserService, HttpUserService>();
+
+builder.Services.AddHostedService<AuthenticationStartupTask>();
+
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+
+T Configure<T>(string sectionName) where T : class
+{
+    var section = builder.Configuration.GetSection(sectionName);
+    var settings = section.Get<T>();
+    builder.Services.Configure<T>(section);
+    return settings;
 }
+
+var app = builder.Build();
+app.UseProblemDetails();
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BackendGestionaleBar v1"));
+app.UseSerilogRequestLogging(options =>
+{
+    options.IncludeQueryInRequestPath = true;
+});
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+await app.RunAsync();
