2af7c3c4a88485c0ae2ee59deb1620a15f9f8ff8
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-06-30 08:56:03 +0200

    updated Length column

--- BarManagerApplication.Backend/src/BackendGestionaleBar.Database/dbo/Tables/Images.sql
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.Database/dbo/Tables/Images.sql
@@ -2,7 +2,7 @@
 (
 	[Id] UNIQUEIDENTIFIER NOT NULL DEFAULT newid(),
 	[Path] NVARCHAR(256) NOT NULL,
-	[Length] INTEGER NOT NULL,
+	[Length] BIGINT NOT NULL,
 	[CreatedDate] DATETIME NOT NULL,
 	[LastModifiedDate] DATETIME NULL,
 
