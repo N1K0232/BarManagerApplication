8aacc5c6835240dfb87436a1a644c5b4abdb4aad
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-06-30 08:56:49 +0200

    updated validator for SaveOrderRequest object

--- BarManagerApplication.Backend/src/BackendGestionaleBar.BusinessLayer/Validators/SaveOrderValidator.cs
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.BusinessLayer/Validators/SaveOrderValidator.cs
@@ -7,12 +7,13 @@ public class SaveOrderValidator : AbstractValidator<SaveOrderRequest>
 {
     public SaveOrderValidator()
     {
-        RuleFor(o => o.IdUser)
-            .NotEqual(Guid.Empty)
-            .WithMessage("Can't save an order without the user");
+        RuleFor(o => o.OrderedQuantity)
+            .NotEqual(0)
+            .GreaterThan(0)
+            .WithMessage("you must specify the quantity");
 
-        RuleFor(o => o.IdProduct)
-            .NotEqual(Guid.Empty)
-            .WithMessage("Can't save an order without the product");
+        RuleFor(o => o.Products)
+            .NotNull()
+            .WithMessage("Can't save an order without the products");
     }
 }
