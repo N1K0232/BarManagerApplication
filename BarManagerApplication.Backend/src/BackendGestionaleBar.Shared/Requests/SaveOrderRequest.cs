cd583a7f8819c2d1684df53265ded17cf903d20e
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-06-30 08:57:24 +0200

    updated SaveOrderRequest

--- BarManagerApplication.Backend/src/BackendGestionaleBar.Shared/Requests/SaveOrderRequest.cs
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.Shared/Requests/SaveOrderRequest.cs
@@ -1,10 +1,14 @@
 ﻿using BackendGestionaleBar.Shared.Common;
+using BackendGestionaleBar.Shared.Enums;
+using BackendGestionaleBar.Shared.Models;
 
 namespace BackendGestionaleBar.Shared.Requests;
 
 public class SaveOrderRequest : BaseRequestObject
 {
-    public Guid IdProduct { get; set; }
+    public int OrderedQuantity { get; set; }
 
-    public Guid IdUser { get; set; }
+    public OrderStatus? Status { get; set; }
+
+    public IEnumerable<Product> Products { get; set; }
 }
