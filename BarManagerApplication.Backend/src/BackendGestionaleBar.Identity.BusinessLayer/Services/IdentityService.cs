2dcabf984884eb4c7210fb122de27fe585d3165c
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-07-01 11:03:44 +0200

    changed indentation

--- BarManagerApplication.Backend/src/BackendGestionaleBar.Identity.BusinessLayer/Services/IdentityService.cs
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.Identity.BusinessLayer/Services/IdentityService.cs
@@ -43,15 +43,15 @@ public class IdentityService : IIdentityService
         var roles = await userManager.GetRolesAsync(user);
 
         var claims = new List<Claim>
-    {
-        new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
-        new Claim(ClaimTypes.Name, user.UserName),
-        new Claim(ClaimTypes.GivenName, user.FirstName),
-        new Claim(ClaimTypes.Surname, user.LastName ?? string.Empty),
-        new Claim(ClaimTypes.DateOfBirth, user.DateOfBirth.ToString()),
-        new Claim(ClaimTypes.Email, user.Email),
-        new Claim(ClaimTypes.MobilePhone, user.PhoneNumber ?? string.Empty)
-    }.Union(roles.Select(role => new Claim(ClaimTypes.Role, role)));
+        {
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Name, user.UserName),
+            new Claim(ClaimTypes.GivenName, user.FirstName),
+            new Claim(ClaimTypes.Surname, user.LastName ?? string.Empty),
+            new Claim(ClaimTypes.DateOfBirth, user.DateOfBirth.ToString()),
+            new Claim(ClaimTypes.Email, user.Email),
+            new Claim(ClaimTypes.MobilePhone, user.PhoneNumber ?? string.Empty)
+        }.Union(roles.Select(role => new Claim(ClaimTypes.Role, role)));
 
         var response = CreateToken(claims);
         await SaveRefreshTokenAsync(user, response.RefreshToken);
