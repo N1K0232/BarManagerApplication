22ef6ced27f69d7deb74288255be41aa81d787e2
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-07-06 21:41:46 +0200

    refactoring

---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.BusinessLayer/Services/Common/BaseService.cs
@@ -0,0 +1,28 @@
+﻿using BackendGestionaleBar.DataAccessLayer;
+using BackendGestionaleBar.DataAccessLayer.Entities.Common;
+using BackendGestionaleBar.Shared.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackendGestionaleBar.BusinessLayer.Services.Common;
+
+public abstract class BaseService
+{
+    private readonly IBarManagerDataContext dataContext;
+
+    protected BaseService(IBarManagerDataContext dataContext)
+    {
+        this.dataContext = dataContext;
+    }
+
+    protected async Task<T> FindAsync<T>(Guid id) where T : BaseEntity
+    {
+        var entity = await dataContext.GetAsync<T>(id);
+        return entity;
+    }
+    protected async Task<T> GetEntity<T>(BaseRequestObject request, bool trackingChanges = false, bool ignoreQueryFilters = false) where T : BaseEntity
+    {
+        var query = dataContext.GetData<T>(trackingChanges, ignoreQueryFilters);
+        var entity = request.Id != null ? await query.FirstOrDefaultAsync(e => e.Id == request.Id) : null;
+        return entity;
+    }
+}
