e08ed571d5fedf8449d77a1fef6d85e081ff9128
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-07-07 19:06:03 +0200

    add IsDeleted and DeletedDate columns

--- BarManagerApplication.Backend/src/BackendGestionaleBar.Database/dbo/Tables/Umbrellas.sql
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.Database/dbo/Tables/Umbrellas.sql
@@ -4,6 +4,8 @@
 	[Coordinates] NVARCHAR(10) NOT NULL,
 	[CreatedDate] DATETIME NOT NULL,
 	[LastModifiedDate] DATETIME NULL,
+	[IsDeleted] BIT NOT NULL,
+	[DeletedDate] DATETIME NULL,
 
 	PRIMARY KEY(Id)
 )
