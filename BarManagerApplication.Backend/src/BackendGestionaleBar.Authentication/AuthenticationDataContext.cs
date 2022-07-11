3a311fb1171a1386510d703a147e2714a026e192
Nicola Silvestri <ns.nicolasilvestri@libero.it>
2022-07-11 08:50:34 +0200

    removed ConfigureConventions, OnConfiguring and SaveChangesAsync overridden methods

--- BarManagerApplication.Backend/src/BackendGestionaleBar.Authentication/AuthenticationDataContext.cs
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.Authentication/AuthenticationDataContext.cs
@@ -20,18 +20,6 @@ public sealed class AuthenticationDataContext
     {
     }
 
-    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
-    {
-        return base.SaveChangesAsync(cancellationToken);
-    }
-    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
-    {
-        base.ConfigureConventions(configurationBuilder);
-    }
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        base.OnConfiguring(optionsBuilder);
-    }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
