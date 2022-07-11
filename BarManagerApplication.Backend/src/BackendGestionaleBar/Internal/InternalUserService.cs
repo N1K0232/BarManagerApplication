a691debd4d21083e3e9456952279302496cf7e0c
Nicola Silvestri <ns.nicolasilvestri@libero.it>
2022-07-11 08:30:20 +0200

    moved file and changed access modifier from public to internal

---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar/Internal/InternalUserService.cs
@@ -0,0 +1,27 @@
+﻿using BackendGestionaleBar.Authentication.Extensions;
+using BackendGestionaleBar.Contracts;
+using System.Security.Claims;
+
+namespace BackendGestionaleBar.Internal;
+
+internal class InternalUserService : IUserService
+{
+    private readonly IHttpContextAccessor httpContextAccessor;
+
+    public InternalUserService(IHttpContextAccessor httpContextAccessor)
+    {
+        this.httpContextAccessor = httpContextAccessor;
+    }
+
+    public ClaimsPrincipal User => httpContextAccessor.HttpContext.User;
+
+    public Guid GetId()
+    {
+        return User.Identity.IsAuthenticated ? User.GetId() : Guid.Empty;
+    }
+
+    public string GetUsername()
+    {
+        return User.Identity.IsAuthenticated ? User.GetUserName() : string.Empty;
+    }
+}
