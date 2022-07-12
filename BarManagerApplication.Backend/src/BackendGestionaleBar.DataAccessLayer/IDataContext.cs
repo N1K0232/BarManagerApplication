a22ecdccb86e785677e24a85f43351bc9cfc0cec
Nicola Silvestri <ns.nicolasilvestri@libero.it>
2022-07-12 14:13:16 +0200

    added spaces between methods

--- BarManagerApplication.Backend/src/BackendGestionaleBar.DataAccessLayer/IDataContext.cs
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.DataAccessLayer/IDataContext.cs
@@ -6,12 +6,19 @@ namespace BackendGestionaleBar.DataAccessLayer;
 public interface IDataContext
 {
     void Delete<T>(T entity) where T : BaseEntity;
+
     void Delete<T>(IEnumerable<T> entities) where T : BaseEntity;
+
     void Edit<T>(T entity) where T : BaseEntity;
+
     IQueryable<T> GetData<T>(bool trackingChanges = false, bool ignoreQueryFilters = false) where T : BaseEntity;
+
     ValueTask<T> GetAsync<T>(params object[] keyValues) where T : BaseEntity;
     Task<List<Menu>> GetMenuAsync();
+
     void Insert<T>(T entity) where T : BaseEntity;
+
     Task SaveAsync();
+
     Task ExecuteTransactionAsync(Func<Task> action);
 }
