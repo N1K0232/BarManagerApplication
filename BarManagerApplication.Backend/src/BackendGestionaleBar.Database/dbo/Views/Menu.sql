95f2b23c11ea5aed4dc63ba648345c966c23f648
Nicola Silvestri <ns.nicolasilvestri@libero.it>
2022-07-12 10:19:17 +0200

    updated database and ViewMenu

--- BarManagerApplication.Backend/src/BackendGestionaleBar.Database/dbo/Views/Menu.sql
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.Database/dbo/Views/Menu.sql
@@ -1,7 +1,6 @@
-﻿
-CREATE VIEW ViewMenu
+﻿CREATE VIEW Menu
 AS
-SELECT p.Name,p.Price,p.Quantity,c.Name as Category
+SELECT p.Name as Product,p.Price,p.Quantity,c.Name as Category
 FROM Products p LEFT JOIN Categories c
 ON p.CategoryId=c.Id
 WHERE p.Quantity > 0
