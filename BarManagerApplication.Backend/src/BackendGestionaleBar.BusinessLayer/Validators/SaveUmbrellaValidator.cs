fc2f0a8c87f2ae7985960820fc103bb200e591e3
Nicola Silvestri <ns.nicolasilvestri@libero.it>
2022-07-15 20:04:34 +0200

    added validator for SaveUmbrellaRequest

---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.BusinessLayer/Validators/SaveUmbrellaValidator.cs
@@ -0,0 +1,20 @@
+﻿using BackendGestionaleBar.Shared.Requests;
+using FluentValidation;
+
+namespace BackendGestionaleBar.BusinessLayer.Validators;
+
+internal class SaveUmbrellaValidator : AbstractValidator<SaveUmbrellaRequest>
+{
+    public SaveUmbrellaValidator()
+    {
+        RuleFor(u => u.Number)
+            .GreaterThanOrEqualTo(100)
+            .WithMessage("add a valid umbrella number");
+
+        RuleFor(u => u.Letter)
+            .NotNull()
+            .NotEmpty()
+            .MaximumLength(1)
+            .WithMessage("insert a valid letter");
+    }
+}
