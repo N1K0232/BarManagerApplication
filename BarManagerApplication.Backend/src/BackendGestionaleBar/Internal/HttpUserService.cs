71d9ff06c3c785c2f87dc76ca9ca7b882c1ca537
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-07-18 10:30:45 +0200

    file rename

---
+++ BarManagerApplication.Backend/src/BackendGestionaleBar/Internal/HttpUserService.cs
@@ -0,0 +1,30 @@
+﻿using BackendGestionaleBar.Authentication.Extensions;
+using BackendGestionaleBar.Contracts;
+
+namespace BackendGestionaleBar.Internal;
+
+#nullable enable
+internal class HttpUserService : IUserService
+{
+    private readonly HttpContext? httpContext;
+
+    public HttpUserService(IHttpContextAccessor httpContextAccessor)
+    {
+        httpContext = httpContextAccessor.HttpContext;
+    }
+
+    public Guid? GetId()
+    {
+        Guid? userId = httpContext?.User.GetId();
+
+        if (userId == Guid.Empty)
+        {
+            return null;
+        }
+
+        return userId;
+    }
+
+    public string? GetUsername() => httpContext?.User.GetUserName();
+}
+#nullable disable
