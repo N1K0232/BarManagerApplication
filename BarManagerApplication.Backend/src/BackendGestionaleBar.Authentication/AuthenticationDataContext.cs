356895c714ff9d6b694b8fdc5ebe222d4da4baba
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-07-25 09:44:44 +0200

    added string converter that trims all the strings

--- BarManagerApplication.Backend/src/BackendGestionaleBar.Authentication/AuthenticationDataContext.cs
+++ BarManagerApplication.Backend/src/BackendGestionaleBar.Authentication/AuthenticationDataContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using System.Reflection;
 
 namespace BackendGestionaleBar.Authentication;
@@ -20,9 +21,25 @@ public sealed class AuthenticationDataContext
     {
     }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    protected override void OnModelCreating(ModelBuilder builder)
     {
-        base.OnModelCreating(modelBuilder);
-        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(builder);
+
+        Assembly assembly = Assembly.GetExecutingAssembly();
+        builder.ApplyConfigurationsFromAssembly(assembly);
+
+        var trimStringConverter = new ValueConverter<string, string>(v => v.Trim(), v => v.Trim());
+        foreach (var entityType in builder.Model.GetEntityTypes())
+        {
+            foreach (var property in entityType.GetProperties())
+            {
+                if (property.ClrType == typeof(string))
+                {
+                    builder.Entity(entityType.Name)
+                        .Property(property.Name)
+                        .HasConversion(trimStringConverter);
+                }
+            }
+        }
     }
 }
